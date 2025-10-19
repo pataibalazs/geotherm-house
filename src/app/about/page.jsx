494f9d773a021cd46a5eab42aad2c50faa064723
balazspatai <therealpataibalazs@gmail.com>
2025-10-19 11:47:07 +0200

    2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -146,7 +146,7 @@ export default function About() {
           </div>
           <ul
             role="list"
-            className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+            className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-2"
           >
             {people.map((person) => (
               <li key={person.name}>
